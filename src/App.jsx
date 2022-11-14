a31c07da006b16a2c785ce2e3eeb74d4587f0138
Christian Boyle <mail@christianboyle.com>
2022-11-14 13:39:23 -0600

    add hof config

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,17 @@ const fetchData = () => {
   return { data: TODOS }
 }
 
-const withConditionalFeedback = (Component) => (props) => {
-  if (props.isLoading) return <div>Loading data.</div>
-  if (!props.data) return <div>No data loaded yet.</div>
-  if (!props.data.length) return <div>Data is empty.</div>
+const withConditionalFeedback =
+  ({ loadingFeedback, noDataFeedback, dataEmptyFeedback }) =>
+  (Component) =>
+  (props) => {
+    if (props.isLoading) return <div>{loadingFeedback || 'Loading data.'}</div>
+    if (!props.data) return <div>{noDataFeedback || 'No data loaded yet.'}</div>
+    if (!props.data.length)
+      return <div>{dataEmptyFeedback || 'Data is empty.'}</div>
 
-  return <Component {...props} />
-}
+    return <Component {...props} />
+  }
 
 const App = () => {
   const { data, isLoading } = fetchData()
@@ -45,6 +49,10 @@ const TodoItem = ({ item }) => {
   )
 }
 
-const TodoList = withConditionalFeedback(BaseTodoList)
+const TodoList = withConditionalFeedback({
+  loadingFeedback: 'Loading Todos.',
+  noDataFeedback: 'No Todos loaded yet.',
+  dataEmptyFeedback: 'Todos are empty.'
+})(BaseTodoList)
 
 export default App
